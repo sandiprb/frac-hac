08cc78c92108af782c1514bebcea26a0db8ecd7c
sandiprb <sandip@coverfoxmail.com>
2017-12-30 20:02:37 +0530

    Fixed index & makefile

--- app/settings.py
+++ app/settings.py
@@ -9,4 +9,5 @@ DB_URI = "mongodb://%s:%s@%s" % (
     quote_plus('fractal'), quote_plus('fractal@123'), 'ds131997.mlab.com:31997/fractal')
 
 
-TEMPLATES_DIR = '../templates'
+TEMPLATES_DIR = 'templates'
+STATIC_DIR = 'static'

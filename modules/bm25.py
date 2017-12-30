e5e36e9ff57d881c4b346062957ebd8bf1082fbe
mohammed <sunasramohd@gmail.com>
2017-12-30 16:57:31 +0530

    Returns sorted dictionary with BM25 scores

--- modules/bm25.py
+++ modules/bm25.py
@@ -2,6 +2,7 @@ from gensim.summarization.bm25 import BM25 as LibBM25
 from nltk.tokenize import word_tokenize
 import pandas as pd
 import operator
+from collections import OrderedDict
 
 class BM25(object):
     """
@@ -17,7 +18,7 @@ class BM25(object):
         self.data = data
         self.query = query
         self.result = self.get_bm25_scores()
-
+        self.best_result = self.getKBest()
 
     def get_bm25_scores(self):
         """
@@ -32,24 +33,23 @@ class BM25(object):
         """
         corpus_df = pd.DataFrame(self.data.values(), index=self.data.keys(), columns=['question'])
         corpus = [word_tokenize(ques) for ques in corpus_df.question]
-        bm25 = LibBM25(corpus)
+        bm25 = BM25(corpus)
         average_idf = sum(float(val) for val in bm25.idf.values()) / float(len(bm25.idf))
         query = word_tokenize(self.query)
         scores = bm25.get_scores(query, average_idf)
         corpus_df['scores'] = scores
         dic = {qid: score for qid, score in zip(corpus_df.index, corpus_df.scores)}
-        sorted_dic = sorted(dic.items(), key=operator.itemgetter(1))
+        sorted_dic = OrderedDict(reversed(sorted(dic.items(), key=lambda t: t[0])))
         return sorted_dic
 
-    def getKBest(self, dic, k=1):
+    def getKBest(self, k=1):
         """
         Selects 'k' best question.reviews after calculating BM25 scores
         :param dic:
         :param k:
         :return:
         """
-        k_best_vals = {key: dic[key] for key in dic.keys()[:k]}
+        k_best_vals = {key: self.result[key] for key in self.result.keys()[:k]}
         return k_best_vals
-
 
 

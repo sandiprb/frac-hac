b26af29058a7dc5c483b5ef45fc29da5688e3884
sandiprb <sandip@coverfoxmail.com>
2017-12-30 14:37:22 +0530

    added utilities

--- app/server/services/queries.py
+++ app/server/services/queries.py
@@ -2,6 +2,16 @@ from mongo import mongo
 from bson import json_util
 import json
 
+
+def cursor_to_list(cursor):
+    result = []
+    for item in cursor:
+        item['id'] = str(item['_id'])
+        item.pop('_id')
+        result.append(item)
+    return result
+
+
 def find_by_textscore(text):
     cursor = mongo.db.QnA.find(
         {'$text': {'$search': text}},
@@ -12,11 +22,14 @@ def find_by_textscore(text):
 
 
 def find_by_asin(asin):
-    cursor = mongo.db.qna.find({"asin": asin})
-    result = []
-    for item in cursor:
-        item['id'] = str(item['_id'])
-        item.pop('_id')
-        result.append(item)
+    cursor = mongo.db.qna.find({'asin': asin})
+    result = cursor_to_list(cursor)
+    return result
 
+
+def find_by_asin_with_textscore(asin='', text=''):
+    cursor = mongo.db.qna.find(
+        {'$text': {'$search': text}, 'asin': asin},
+        {'score': {'$meta': 'textScore'}})
+    result = cursor_to_list(cursor)
     return result

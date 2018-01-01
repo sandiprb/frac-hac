c269804c7b583a1a2976da82b81310ab2a205e78
mohammed <sunasramohd@gmail.com>
2018-01-01 21:05:29 +0530

    Sentiment analysis on answer

--- modules/sentiment.py
+++ modules/sentiment.py
@@ -8,8 +8,8 @@ NEUTRAL = 0
 
 class Sentiment(object):
 
-    def __init__(self, reviews):
-        self.reviews = reviews
+    def __init__(self, data):
+        self.data = data
 
     def get_reviews_sentiment(self):
         """
@@ -17,43 +17,48 @@ class Sentiment(object):
         :param reviews:
         :return:
         """
-        reviews_df = pd.DataFrame(self.reviews)
+        reviews_df = pd.DataFrame(self.data)
         reviews_df['sentiment_score'] = 0
         reviews_df['sentiment_type'] = 0
-        reviews_df[['sentiment_score',  'sentiment_type']] = reviews_df.apply(self.calculate_sentiment, axis=1)
+        key = 'reviewText'
+        reviews_df[['sentiment_score', 'sentiment_type']] = reviews_df.apply(self.calculate_sentiment, args=([key]), axis=1)
 
         top_3_reviews = self.get_diverse_reviews(reviews_df)
 
         return top_3_reviews
 
 
-    def get_answer_sentiment(self, answer):
+    def get_answers_sentiment(self):
         """
-        Calculates sentiment of the most relevant Answer found
-        :param answer:
+        Calculates Sentiment analysis of all reviews/asnwers found and return best matched results
+        :param reviews:
         :return:
         """
-        answer_text = answer['answer']
-        sent_score = TextBlob(answer_text).sentiment.polarity
-        answer['sentiment_score'] = sent_score
+        answer_df = pd.DataFrame(self.data)
+        answer_df['sentiment_score'] = 0
+        answer_df['sentiment_type'] = 0
+        key = 'answer'
+        answer_df[['sentiment_score', 'sentiment_type']] = answer_df.apply(self.calculate_sentiment, args=([key]), axis=1)
 
-        if(sent_score > 0):
-            answer['sentiment_type'] = POSITIVE
-        elif (sent_score < 0):
-            answer['sentiment_type'] = NEGATIVE
+        answer_df = answer_df.sort_values(by='bm25_score', ascending=False)
+
+        if answer_df.empty:
+            best_answer = {}
         else:
-            answer['sentiment_type'] = NEUTRAL
-        return answer
+            best_answer = answer_df.iloc[0].to_dict()
 
-    def calculate_sentiment(self, review):
+        return best_answer
+
+
+    def calculate_sentiment(self, data, key):
         """
         Calculates sentiment of a reviewText and returns Sentiment score and Sentiment Type
-        :param review:
+        :param data:
         :return:
         """
 
-        review_text = review['reviewText']
-        senti_score = TextBlob(review_text).sentiment.polarity
+        data_text = data[key]
+        senti_score = TextBlob(data_text).sentiment.polarity
         if (senti_score > 0):
             senti_type = POSITIVE
         elif (senti_score < 0):
@@ -62,6 +67,7 @@ class Sentiment(object):
             senti_type = NEUTRAL
 
         return pd.Series({'sentiment_score': senti_score, 'sentiment_type': senti_type})
+
 
     def get_diverse_reviews(self, reviews_data):
         """

da1d1ff0583971992e212e0dd4f6ff810871c32b
mohammed <sunasramohd@gmail.com>
2018-01-03 21:44:24 +0530

    Moving spacy import thing on class level

--- modules/getdocuments.py
+++ modules/getdocuments.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import spacy
 
+nlp = spacy.load('en')
 
 class Document(object):
 
@@ -19,20 +20,6 @@ class Document(object):
         self.column = column
         # self.documents = self.get_documents()
 
-    def __get_documents(self):
-        """
-        Fetches all the documents returned by DB for a PID
-        And creates a dict of key(id) and value(question)
-        :return:
-        """
-        data = {}
-        documents = self.data
-        for line in documents:
-            doc_id = line['id']
-            question = line['question']
-            data[doc_id] = question
-        return data
-
     def get_similar_documents(self, threshold=0.5):
         """
         Returns similar documents for the query with similarity score greater than the threshold value
@@ -42,7 +29,6 @@ class Document(object):
         :return: Request data
         :rtype: dict
         """
-        nlp = spacy.load('en')
         docs = self.data[self.column].apply(nlp)
         query = nlp(self.query)
         self.data['similarity_score'] = docs.apply(query.similarity)

fe5b8efb5184bf442e7b4360cd830b1d2c8023b9
sandiprb <sandip@coverfoxmail.com>
2018-01-03 15:36:33 +0530

    Answer fetching error handling & major UI changes

--- app/api/controllers.py
+++ app/api/controllers.py
@@ -14,24 +14,27 @@ def find_by_pid():
     request_data = request.get_json()
     raw_query = request_data['query']
     pid = request_data['pid']
-    relevant_question = utils.get_most_relevant_question(raw_query, pid)
+    response = {}
+    if pid and raw_query:
+        relevant_question = utils.get_most_relevant_question(raw_query, pid)
+        # ToDO: Move the following reviews to a seprate view & fire two requests from front-end
+        relevant_reviews = utils.get_most_relevant_reviews(raw_query, pid)
 
-    # ToDO: Move the following reviews to a seprate view & fire two requests from front-end
-    relevant_reviews = utils.get_most_relevant_reviews(raw_query, pid)
+        question_setiment = utils.get_question_setiment(relevant_question)
+        reviews_sentiment = utils.get_reviews_sentiment(relevant_reviews)
 
-    question_setiment = utils.get_question_setiment(relevant_question)
-    reviews_sentiment = utils.get_reviews_sentiment(relevant_reviews)
+        response['success'] = True
+        response['data'] = question_setiment
+        response['reviews'] = reviews_sentiment
 
-    result = {}
-    result['success'] = True
-    result['data'] = question_setiment
-    result['reviews'] = reviews_sentiment
+        if not question_setiment:
+            response['success'] = False
+            response['data'] = 'No data found!'
 
-    if not question_setiment:
-        result['success'] = False
-        result['data'] = 'No data found!'
+        if not reviews_sentiment:
+            response['reviews'] = 'No data found!'
+    else:
+        response['success'] = False
+        response['error'] = 'Please enter a valid search query'
 
-    if not reviews_sentiment:
-        result['reviews'] = 'No data found!'
-
-    return jsonify(result)
+    return jsonify(response)
--- app/static/src/components/Answer.tsx
+++ app/static/src/components/Answer.tsx
@@ -1,65 +1,36 @@
-import * as React from "react";
-/* import ContentLoader from "react-content-loader" */
+import * as React from 'react'
 
-import { IAnswer } from "../interface";
-
-/* const AnswerLoader = () => (
-	<ContentLoader
-		height={200}
-		width={505}
-		speed={2}
-		primaryColor={"#f3f3f3"}
-		secondaryColor={"#ecebeb"}
-	>
-		<rect x="25" y="15" rx="5" ry="5" width="680" height="10" />
-		<rect x="25" y="45" rx="5" ry="5" width="400" height="10" />
-		<rect x="585" y="-45" rx="5" ry="5" width="420" height="10" />
-		<rect x="141" y="71" rx="5" ry="5" width="100" height="10" />
-		<rect x="26" y="72" rx="5" ry="5" width="100" height="10" />
-	</ContentLoader>
-) */
+import { IAnswer } from '../interface'
 
 interface IProps {
-  answer: IAnswer;
-  isFetching: boolean;
+	answer: IAnswer
 }
 
-export const AnswerCard = ({ isFetching, answer }: IProps) => {
-  return (
-    <div className="answer-wrapper">
-      {answer && <p>Based on your query, we Found following question!</p>}
-      <div className="answer-card">
-        {isFetching ? (
-          <div />
-        ) : (
-          <div>
-            <h5>{answer.question}</h5>
-            <h6 style={{ opacity: 0.7 }}>{answer.answer}</h6>
-            <hr />
-            <div className="answer-card__footer">
-              {answer.asin && (
-                <div>
-                  {" "}
-                  <span className="key"> Asin: </span> {answer.asin}{" "}
-                </div>
-              )}
-              {answer.answerTime && (
-                <div>
-                  <span className="key"> Answer Time: </span>{" "}
-                  {answer.answerTime}{" "}
-                </div>
-              )}
-              {answer.questionType && (
-                <div>
-                  {" "}
-                  <span className="key"> Question Type:</span>
-                  {answer.questionType}{" "}
-                </div>
-              )}
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+export const AnswerCard = ({ answer }: IProps) => {
+	return (
+		<section className="section">
+			<div className="answer-wrapper unit-appear-up">
+				<div className="answer-card">
+					<div>
+						<h5>{answer.question}</h5>
+						<h6 style={{ opacity: 0.7 }}>{answer.answer}</h6>
+						<hr />
+						<div className="answer-card__footer">
+							{answer.asin && (
+								<div>
+									{' '}
+									<span className="key"> Asin: </span> {answer.asin}{' '}
+								</div>
+							)}
+							{answer.answerTime && (
+								<div>
+									<span className="key"> Answer Time: </span> {answer.answerTime}{' '}
+								</div>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}

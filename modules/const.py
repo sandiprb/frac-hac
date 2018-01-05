b5ffbbcb99a7b6b77919fe01968672999cf2c604
Siddharth Pant <spsiddharthpant@gmail.com>
2018-01-05 11:40:01 +0530

    Added a constant class in module const.py

---
+++ modules/const.py
@@ -0,0 +1,15 @@
+class CONST(object):
+    """
+    Constant values that raise TypeError when one tries to change them
+    """
+    COL_SCORE = 'sentiment_score'
+    COL_TYPE = 'sentiment_type'
+    COL_REVIEW = 'reviewText'
+    COL_BM25 = 'bm25_score'
+    COL_DELETE = 'delte_flag'
+    COL_SIMILAR = 'similarity_score'
+
+    def __setattr__(self, *_):
+        raise TypeError('Cannot change CONST values')
+
+# CONST = CONST()

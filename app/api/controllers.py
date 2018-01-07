992d5bfb068dddcd9d272705732fe70fc1befa31
sandiprb <sandip@coverfoxmail.com>
2018-01-07 10:09:41 +0530

    API Response fix

--- app/api/controllers.py
+++ app/api/controllers.py
@@ -17,11 +17,7 @@ def find_by_pid():
     response = {}
     if pid and raw_query:
         scored_question = utils.get_most_relevant_question(raw_query, pid)
-        # ToDO: Move the following reviews to a seprate view & fire two requests from front-end
         scored_reviews = utils.get_most_relevant_reviews(raw_query, pid)
-
-        # answer_sentiment = utils.get_answer_sentiment(relevant_question) if relevant_question else None
-        # reviews_sentiment = utils.get_reviews_sentiment(relevant_reviews) if relevant_reviews else None
 
         response['success'] = True
         response['data'] = scored_question
@@ -30,9 +26,9 @@ def find_by_pid():
         if not scored_question:
             response['success'] = False
             response['data'] = None
+            response['error'] = 'No similar questions found'
 
         if not scored_reviews:
-            response['success'] = False
             response['reviews'] = None
 
     else:
